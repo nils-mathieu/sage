261acf20e6fd0b3e88ff5282a455edcd8eddf9c5
Nils Mathieu <nils.mathieu.contact@gmail.com>
2023-02-12 20:38:22 +0100

    initial setup of `sage-platform-core`

---
+++ crates/sage-platform-core/src/lib.rs
@@ -0,0 +1,6 @@
+//! Defines core primitive types and traits for to try and remain generic over the platform that
+//! hosts our applications.
+//!
+//! Specifically, this crate defines how to represent the state and the transition of popular input
+//! devices such as the keyboard, the mouse, or gamepads.
+// TODO: when we have those implemented, add links on "keyboard", "mouse", and "gamepad".

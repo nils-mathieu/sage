b4fd7a610400c6ee7309e4207439b86493e192a7
Nils <nils.mathieu.contact@gmail.com>
2023-02-14 23:42:46 +0100

    add the `sage-render` crate

---
+++ crates/sage-render/src/lib.rs
@@ -0,0 +1,4 @@
+//! Provides a type-safe and low-levelish API to interact with the Vulkan API.
+//!
+//! This library does not try to make everything safe. Instead, its goal is to make it easy to use
+//! the Vulkan API correctly and efficiently.
